dd353a4724aa1934b9008e57a63df714d00cc54f
David Jimenez <david.jimenez@anz.com>
2020-03-25 16:03:48 +1100

    Add GetPrimes function

--- prime_gap/main.go
+++ prime_gap/main.go
@@ -8,6 +8,14 @@ func main() {
 	fmt.Println("Hallo du schöne Welt!")
 }
 
-func Gap(n int) []int {
+// Prime Gap function, g and m are the limits and n is the target
+func Gap(g, m, n int) []int {
 	return []int{1, 1}
+}
+
+// GetPrimes, get prime numbers up to the number given
+func GetPrimes(n int) []int {
+	var primes []int
+	primes = []int{2, 3, 5}
+	return primes
 }
--- prime_gap/main_test.go
+++ prime_gap/main_test.go
@@ -7,17 +7,36 @@ import (
 
 func TestGap(t *testing.T) {
 	tables := []struct {
-		x int
+		x [3]int
 		n []int
 	}{
-		{0, []int{}},
-		{3, []int{2, 2}},
+		{[3]int{0, 1, 0}, []int{}},
+		{[3]int{0, 5, 2}, []int{3, 5}},
 	}
 
 	for _, table := range tables {
 
 		expected := table.n
-		actual := Gap(table.x)
+		actual := Gap(table.x[0], table.x[1], table.x[2])
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("Gap of (%d) was incorrect, got: %d, want: %d.", table.x, actual, expected)
+		}
+	}
+}
+
+func TestGetPrimes(t *testing.T) {
+	tables := []struct {
+		x int
+		n []int
+	}{
+		{2, []int{}},
+		{5, []int{2, 3, 5}},
+	}
+
+	for _, table := range tables {
+
+		expected := table.n
+		actual := GetPrimes(5)
 		if !reflect.DeepEqual(actual, expected) {
 			t.Errorf("Gap of (%d) was incorrect, got: %d, want: %d.", table.x, actual, expected)
 		}

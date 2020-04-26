3256dd26c2244934ed7841c1105cffb4544d5e56
David Jimenez <david.jimenez@anz.com>
2020-04-26 16:34:54 +1000

    implement solution with loop

--- string_problems/main.go
+++ string_problems/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"sort"
+	"strings"
+)
+
 func IsUnique(s string) bool {
-	return false
+	a := strings.Split(s, "")
+	result := true
+	sort.Strings(a)
+	for i := 0; i < len(a)-1; i++ {
+		if a[i] == a[i+1] {
+			result = false
+		}
+	}
+	return result
+}
+
+func IsUniqueWithLoop(s string) bool {
+	a := strings.Split(s, "")
+	result := true
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a); j++ {
+			if i != j {
+				if a[i] == a[j] {
+					result = false
+				}
+			}
+		}
+	}
+	return result
 }
--- string_problems/main_test.go
+++ string_problems/main_test.go
@@ -4,18 +4,40 @@ import (
 	"testing"
 )
 
-func TestIsUnique(t *testing.T) {
-	tables := []struct {
+var (
+	tables = []struct {
 		x string
 		n bool
 	}{
 		{"abc", true},
+		{"abb", false},
+		{"", true},
+		{"♛", true},
+		{"♛♛♛", false},
+		{"", true},
+		{" ", true},
+		{"  ", false},
+		{"111", false},
+		{"123", true},
 	}
+)
 
+// test if a string has all unique characters
+func TestIsUnique(t *testing.T) {
 	for _, table := range tables {
-
 		expected := table.n
 		actual := IsUnique(table.x)
+		if actual != expected {
+			t.Errorf("Test of (%s) was incorrect, got: %v, want: %v.", table.x, actual, expected)
+		}
+	}
+}
+
+// test if a string has all unique characters with loop implementation
+func TestIsUniqueWithLoop(t *testing.T) {
+	for _, table := range tables {
+		expected := table.n
+		actual := IsUniqueWithLoop(table.x)
 		if actual != expected {
 			t.Errorf("Test of (%s) was incorrect, got: %v, want: %v.", table.x, actual, expected)
 		}
